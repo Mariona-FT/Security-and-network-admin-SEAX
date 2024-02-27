d81a396f19606276a7ecfbb4a3cd66333e4da711
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-02-27 01:55:36 +0100

    + funcions q faltaven + trafic

--- info_inet.sh
+++ info_inet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec > test.log 2>&1
+#exec > test.log 2>&1
 
 print_horizontal_line() {
     printf "┌"
@@ -71,8 +71,9 @@ get_max_length() {
 
     #Adreça mac - $mac
     funcio_mac() {
+        #Ip link per buscar la mac de la interficie donada
        mac=$( ip link show "$1" | grep "link/" |awk '{printf $2}' )
-        if [ -z "$mac" ]; then
+        if [ -z "$mac" ]; then  # Si retorna una mac buida
             echo "Cap mac trobada"
         else 
             echo "$mac"
@@ -81,8 +82,9 @@ get_max_length() {
 
     #Estat interficie - $estat_interficie
     funcio_estat(){
+        #Ip addr per buscar lestat de la maquina UP o DOWN
         estat=$(ip addr show "$1" 2>/dev/null | grep -Po 'state \K[^ ]+')
-        if [ -z "$estat" ] || [ "$estat" == "DOWN" ]; then
+        if [ -z "$estat" ] || [ "$estat" == "DOWN" ]; then #Depenen estat echo diferents 
             echo "DOWN (no responent...)"
         else
             echo "UP (responent...)"
@@ -95,8 +97,50 @@ get_max_length() {
          mtu=$(cat /sys/class/net/$interficie/mtu)
     }
 
+    # adreçament - $adrecament
+    funcio_adrecament() {
+        adrecament=$
+    }
+
+    #Adreca ip i mascara - $ip_masc
+    funcio_ip_mascara() {
+        adip_masc=$
+    }
+
+    #Adreca de xarxa - $adxarxa
+    funcio_xarxa() {
+        adxarxa=$
+    }
+
+    #Adreça de broadcast - $broadcast
+    funcio_broadcast() {
+        broadcast=$
+    }
+
+    #Adreça gateway per defecte - $gateway
+    funcio_gateway() {
+        gateway=$
+    }
+
+    #Nom dns - $nom_dns
+    funcio_dns_nom() {
+        nom_dns=$
+    }
+
+    funcio_trafic(){
+        interface=$1
+        # Get traffic information using ifconfig
+        rx_bytes=$(ip -s link show $interface | awk '/RX:/{print $2}' | cut -d' ' -f1)
+        tx_bytes=$(ip -s link show $interface | awk '/TX:/{print $2}' | cut -d' ' -f1)
+        rx_packets=$(ip -s link show $interface | awk '/RX:/{print $1}' | cut -d' ' -f1)
+        tx_packets=$(ip -s link show $interface | awk '/TX:/{print $1}' | cut -d' ' -f1)
+        # Return traffic information as an array
+        echo "$rx_bytes $tx_bytes $rx_packets $tx_packets"
+    }
 
 # Llistat de totes les interficies de la maquina
+
+
 for interficie in $(ls /sys/class/net); do
 
     # TITOL INTERFÍCIE
@@ -108,7 +152,18 @@ for interficie in $(ls /sys/class/net); do
     estat=$(funcio_estat $interficie)
     mode_interficie=$(funcio_mode $interfice)
 
-   # mac=$(cat /sys/class/net/$interficie/address)
+    adrecament=$(funcio_adrecament $interfice)
+    ip_masc=$(funcio_ip_mascara $interficie)
+    adxarxa=$(funcio_xarxa $interficie)
+    broadcast=$(funcio_broadcast $interficie)
+    gateway=$(funcio_gateway $interficie)
+    nom_dns=$(funcio_dns_nom $interficie)
+
+    trafic_info=($(funcio_trafic $interficie))
+    t_rebut=${trafic_info[0]}
+    t_transmes=${trafic_info[1]}
+    vel_recep=${trafic_info[2]}
+    vel_trans=${trafic_info[3]}
 
     # Print dels resultats
     resultats=(
@@ -121,12 +176,12 @@ for interficie in $(ls /sys/class/net); do
         "Mode de la interfície:     $mode_interficie"
        
         ""
-        "Adreçament:                Unknown"
-        "Adreça IP / màscara:       Unknown"
-        "Adreça de xarxa:           Unknown"
-        "Adreça broadcast:          Unknown"
-        "Gateway per defecte:       Unknown"
-        "Nom DNS:                   Unknown"
+        "Adreçament:                $adrecament"
+        "Adreça IP / màscara:       $ip_masc"
+        "Adreça de xarxa:           $adxarxa"
+        "Adreça broadcast:          $broadcast"
+        "Gateway per defecte:       $gateway"
+        "Nom DNS:                   $nom_dns"
        
         ""
 
@@ -136,12 +191,12 @@ for interficie in $(ls /sys/class/net); do
         "Xarxes de l'entitat:       "
         "Entitat propietària:       "
        
-        "" 
+        "" #Possible ruta involucrada
 
-        "Tràfic rebut:              Unknown"
-        "Tràfic transmès:           Unknown"
-        "Velocitat de Recepció:     Unknown"
-        "Velocitat de Transmissió:  Unknown"
+        "Tràfic rebut:              $t_rebut"
+        "Tràfic transmès:           $t_transmes"
+        "Velocitat de Recepció:     $vel_recep"
+        "Velocitat de Transmissió:  $vel_trans"
     )
 
     # Determine the max length of the details
