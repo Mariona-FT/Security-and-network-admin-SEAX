649007a2a06f6826b23f1034dba46c8d9c347e1e
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-03-05 16:58:10 +0100

    TAULA valors correctes
    + inicialitzacions
    + comentaris

--- sessio3/proves_taula/taula.sh
+++ sessio3/proves_taula/taula.sh
@@ -8,76 +8,79 @@ echo " -------------------------------------------------------------------------
 echo "       SSID          canal  freqüència    senyal     v. max.   xifrat    algorismes xifrat       Adreça MAC           fabricant        " >> taula.log
 echo " ------------------------------------------------------------------------------------------------------------------------------------- " >> taula.log 
 
-# Function to process the input file
-# Function to process the input file
-# Function to process the input file
-# Function to process the input file
 process_file() {
     input_file="log_scan.txt"
     output_file="taula.log"
 
-    # Initialize variables to store BSS information
-    declare -A bss_info
+    #Inicialitzar vector de informacio de la xarxa
+    declare -A xarxa
     keys=("admac" "ssid" "channel" "freq" "signal" "v_max" "xif" "al_xif" "fab")
-    # for key in "${keys[@]}"; do
-    #     bss_info["$key"]=""
-    # done
     info_rsn=0
 
-    # Read each line from the input file
     while IFS= read -r line; do
-        # Check if the line contains a BSS entry
+        # Si linia comenca amb BSS - Nova xarxa 
         if [[ $line =~ BSS\ ([0-9a-fA-F:]+) ]]; then
-            # If not the first BSS entry, print previous BSS information
-            if [ ${bss_info[admac]+_} ]; then
-            echo -e "${bss_info[ssid]}\t\t ${bss_info[channel]}\t${bss_info[freq]} MHz ${bss_info[signal]} dBm ${bss_info[v_max]} Mbps\t ${bss_info[xif]}\t ${bss_info[al_xif]}\t\t ${bss_info[admac]}\t ${bss_info[fab]}" >> "$output_file"
-
+            # Si no es la primera xarxa trobada - treu la informació de l'antiga xarxa
+            if [ ${xarxa[admac]+_} ]; then
+            echo -e "${xarxa[ssid]}\t\t ${xarxa[channel]}\t${xarxa[freq]} MHz ${xarxa[signal]} dBm \t ${xarxa[v_max]} Mbps\t ${xarxa[xif]}\t ${xarxa[al_xif]}\t\t ${xarxa[admac]}\t ${xarxa[fab]}" >> "$output_file"
             fi
-            # Clear the BSS information for the new entry
-            unset bss_info
-            declare -A bss_info
-            bss_info["admac"]=${BASH_REMATCH[1]}
-        elif [[ $line =~ freq:\ ([0-9.]+) ]]; then
-            bss_info["freq"]=${BASH_REMATCH[1]}
+            # Netejar els valors i donar valors inicials
+            unset xarxa
+            declare -A xarxa
+            for key in "${keys[@]}"; do
+            case "$key" in
+                "xif")
+                    xarxa["$key"]="sense" ;;
+                *)
+                    xarxa["$key"]="." ;;
+            esac
+        done
+        #Troba la mac de la xarxa 
+            xarxa["admac"]=${BASH_REMATCH[1]}
+        #Troba el ssid de la xarxa
         elif [[ $line =~ SSID:\ (.+) ]]; then
-            bss_info["ssid"]=${BASH_REMATCH[1]}
-        elif [[ $line =~ signal:\ (-[0-9]+) ]]; then
-            bss_info["signal"]=${BASH_REMATCH[1]}
-        elif [[ $line =~ ' * Manufacturer: '([[:alnum:]]+) ]]; then
-            bss_info["fab"]=${BASH_REMATCH[1]}
+            xarxa["ssid"]=${BASH_REMATCH[1]} 
+        #Troba el num del canal de la xarxa
         elif [[ $line =~ ' * primary channel: '([[:alnum:]]+) ]]; then
-            bss_info["channel"]=${BASH_REMATCH[1]}
-        elif [[ $line =~ 'Maximum RX AMPDU length' ]]; then
-            echo "aa"
-            bss_info["v_max"]=$(grep -oP 'Maximum RX AMPDU length \K[0-9]+' <<< "$line")
+            xarxa["channel"]=${BASH_REMATCH[1]}
+        #Troba frequencia de la xarxa 
+        elif [[ $line =~ freq:\ ([0-9.]+) ]]; then
+            xarxa["freq"]=${BASH_REMATCH[1]}
+        #Troba la senyal de la xarxa
+        elif [[ $line =~ signal:\ (-[0-9]+) ]]; then
+            xarxa["signal"]=${BASH_REMATCH[1]}
+        #Troba la velocitat maxima de la xarxa
+        elif [[ $line =~ 'Supported rates:' ]]; then
+            vm=$(grep -oP '\b[0-9]+(\.[0-9]+)?(?=\s*\*?\s*$)' <<< "$line" | tail -n 1)
+            xarxa["v_max"]=$vm
+        #Troba el xifrat de la xarxa
+        elif [[ $line =~ 'WEP' || $line =~ 'WPA' || $line =~ 'TKIP' || $line =~ 'AES' ]]; then
+                    xarxa["xif"]=""
+                    encryption_method=$(grep -oP '(WEP|WPA|TKIP|AES)' <<< "$line")
+                    wpa_version=$(grep -oP '\b[0-9]+(\.[0-9]+)?(?=\s*\*?\s*$)' <<< "$line" | tail -n 1) #treure versio
+                    encryption_method+="$wpa_version"
+                    xarxa["xif"]+="$encryption_method"        
+        #Troba els algorismes xifrats - estaran despres de la linia RSN  - sino no son els algorismes de la xarxa
         elif [[ $line =~ 'RSN:' ]]; then
             info_rsn=1
-            echo "rsn" $info_rsn
-        elif [[ $line =~ 'Group cipher: '(.+) && $info_rsn -eq 1 ]]; then
-            bss_info["al_xif"]+="${BASH_REMATCH[1]}-"
-        elif [[ $line =~ 'Pairwise ciphers: '(.+) && $info_rsn -eq 1 ]]; then
-            bss_info["al_xif"]+="${BASH_REMATCH[1]}-"
-        elif [[ $line =~ 'Authentication suites: '(.+) && $info_rsn -eq 1 ]]; then
-            bss_info["al_xif"]+="${BASH_REMATCH[1]}"
+            xarxa["al_xif"]=""
+        elif [[ $line =~ 'Group cipher: '(.+) && $info_rsn -eq 1 ]]; then #si abans linia RSN - si algorisme xifrat
+            xarxa["al_xif"]="${BASH_REMATCH[1]}-"
+        elif [[ $line =~ 'Pairwise ciphers: '(.+) && $info_rsn -eq 1 ]]; then #si abans linia RSN - si algorisme xifrat
+            xarxa["al_xif"]+="${BASH_REMATCH[1]}-"
+        elif [[ $line =~ 'Authentication suites: '(.+) && $info_rsn -eq 1 ]]; then #si abans linia RSN - si algorisme xifrat
+            xarxa["al_xif"]+="${BASH_REMATCH[1]}"
             info_rsn=0
-        
-        elif [[ $line =~ 'WEP' || $line =~ 'WPA' || $line =~ 'TKIP' || $line =~ 'AES' ]]; then
-            echo "bb"
-            encryption_method=$(grep -oP '(WEP|WPA|TKIP|AES)' <<< "$line")
-            if [[ $line =~ 'WPA: *Version: ([0-9]+)' ]]; then
-                wpa_version=${BASH_REMATCH[1]}
-                encryption_method+=" WPA$wpa_version"
-            fi
-            bss_info["xif"]+="$encryption_method "
-        fi
+        #Troba el fabricant de la xarxa
+        elif [[ $line =~ ' * Manufacturer: '([[:alnum:]]+) ]]; then
+            xarxa["fab"]=${BASH_REMATCH[1]}
+fi
     done < "$input_file"
 
     # Print the information of the last BSS entry
-    if [ ${bss_info[admac]+_} ]; then
-        echo -e "${bss_info[ssid]}\t\t ${bss_info[channel]}\t${bss_info[freq]} MHz  ${bss_info[signal]} dBm ${bss_info[v_max]} Mbps\t ${bss_info[xif]}\t ${bss_info[al_xif]}\t\t ${bss_info[admac]}\t ${bss_info[fab]}" >> "$output_file"
+    if [ ${xarxa[admac]+_} ]; then
+            echo -e "${xarxa[ssid]}\t\t ${xarxa[channel]}\t${xarxa[freq]} MHz ${xarxa[signal]} dBm \t ${xarxa[v_max]} Mbps\t ${xarxa[xif]}\t ${xarxa[al_xif]}\t\t ${xarxa[admac]}\t ${xarxa[fab]}" >> "$output_file"
     fi
-
-    echo "Processing complete. Output saved to $output_file"
 }
 
 
