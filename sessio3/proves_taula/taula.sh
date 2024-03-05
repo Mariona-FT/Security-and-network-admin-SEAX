ff66cd50c77546b45bf2f939c1a6589cfd293e4f
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-03-05 11:37:40 +0100

    proves taula - funciona x cada BSS trobada

--- sessio3/proves_taula/taula.sh
+++ sessio3/proves_taula/taula.sh
@@ -9,49 +9,63 @@ echo "       SSID          canal  freqüència    senyal     v. max.   xifrat   
 echo " ------------------------------------------------------------------------------------------------------------------------------------- " >> taula.log 
 
 # Function to process the input file
+# Function to process the input file
+# Function to process the input file
+# Function to process the input file
 process_file() {
     input_file="log_scan.txt"
     output_file="taula.log"
 
+    # Initialize variables to store BSS information
+    declare -A bss_info
+    keys=("admac" "ssid" "channel" "freq" "signal" "v_max" "xif" "sta_channel_width" "fab")
+
     # Read each line from the input file
-   while IFS= read -r line; do
-        # Check if the line contains BSS and a MAC address
+    while IFS= read -r line; do
+        # Check if the line contains a BSS entry
         if [[ $line =~ BSS\ ([0-9a-fA-F:]+) ]]; then
-            admac=$line
-            # Initialize variables to store information
-            ssid=""
-            channel=""
-            freq=""
-            signal=""
-            v_max=""
-            xif=""
-            sta_channel_width=""
-            fab=""
-            # Extract other information for the current BSS entry
-            while read -r subline; do
-                if [[ $subline =~ freq:\ ([0-9.]+) ]]; then
-                    freq=${BASH_REMATCH[1]}
-                elif [[ $subline =~ SSID:\ (.+) ]]; then
-                    ssid=${BASH_REMATCH[1]}
-                elif [[ $subline =~ signal:\ (-[0-9]+) ]]; then
-                    signal=${BASH_REMATCH[1]}
-                fi
-            done <<< "$line"
-            # Extract additional information
-            if [[ $line =~ HT\ operation:\  ]]; then
-                al_xif=${line#*: }
-                v_max=$(grep -oP 'Maximum RX AMPDU length \K[0-9]+' <<< "$al_xif")
-                xif=$(grep -oP 'Supported rates: \K[0-9]+.[0-9]+' <<< "$al_xif")
-                fab=$(grep -oP 'Manufacturer: \K\w+' <<< "$al_xif")
-                channel=$(grep -oP 'primary channel: \K\d+' <<< "$al_xif")
-                sta_channel_width=$(grep -oP 'STA channel width: \K\S+' <<< "$al_xif")
+            # If not the first BSS entry, print previous BSS information
+            if [ ${bss_info[admac]+_} ]; then
+                print_bss_info
             fi
-            # Echo the collected information for the current BSS entry
-            echo -e " HOLA $ssid\t$channel\t$freq\t$signal dBm\t$v_max\t$xif\t$sta_channel_width\t$admac\t$fab" >> "$output_file"
+            # Clear the BSS information for the new entry
+            unset bss_info
+            declare -A bss_info
+            bss_info["admac"]=${BASH_REMATCH[1]}
+        elif [[ $line =~ freq:\ ([0-9.]+) ]]; then
+            bss_info["freq"]=${BASH_REMATCH[1]}
+        elif [[ $line =~ SSID:\ (.+) ]]; then
+            bss_info["ssid"]=${BASH_REMATCH[1]}
+        elif [[ $line =~ signal:\ (-[0-9]+) ]]; then
+            bss_info["signal"]=${BASH_REMATCH[1]}
+        elif [[ $line =~ ' * Manufacturer: '([[:alnum:]]+) ]]; then
+            bss_info["fab"]=${BASH_REMATCH[1]}
+        elif [[ $line =~ ' * primary channel: '([[:alnum:]]+) ]]; then
+            bss_info["channel"]=${BASH_REMATCH[1]}
+        elif [[ $line =~ ' Maximum RX AMPDU length' ]]; then
+            bss_info["v_max"]=${BASH_REMATCH[1]}
+        
+        elif [[ $line =~ 'HT\ operation:\'  ]]; then
+                    al_xif=${line#*: }
+            #bss_info["v_max"]=$(grep -oP' \K[0-9]+' <<< "$al_xif")
+            bss_info["xif"]=$(grep -oP 'Supported rates: \K[0-9]+.[0-9]+' <<< "$al_xif")
+           # bss_info["fab"]=$(grep -oP 'Manufacturer: \K\w+' <<< "$al_xif")
+           # bss_info["channel"]=$(grep -oP ' * primary channel: \K\d+' <<< "$al_xif")
+            bss_info["sta_channel_width"]=$(grep -oP 'STA channel width: \K\S+' <<< "$al_xif")
         fi
     done < "$input_file"
 
+    # Print the information of the last BSS entry
+    if [ ${bss_info[admac]+_} ]; then
+        print_bss_info
+    fi
+
     echo "Processing complete. Output saved to $output_file"
+}
+
+# Function to print BSS information
+print_bss_info() {
+    echo -e " HOLA ${bss_info[ssid]}\t${bss_info[channel]}\t${bss_info[freq]}\t${bss_info[signal]} dBm\t${bss_info[v_max]}\t${bss_info[xif]}\t${bss_info[sta_channel_width]}\t${bss_info[admac]}\t${bss_info[fab]}" >> "$output_file"
 }
 
 # Call the function to process the input file
